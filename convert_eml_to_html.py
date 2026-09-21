74fbebe1b96b6fb6005cf0d4bdbfde7f37223085
Baebibelab <204586638+Baebibelab@users.noreply.github.com>
2026-09-21 18:31:23 +0200

    Gestion des caractères spéciaux et des images

--- convert_eml_to_html.py
+++ convert_eml_to_html.py
@@ -1,12 +1,24 @@
 import email
 from email.policy import default
 import os
+import base64
 
 def eml_to_html(eml_file, html_file):
     with open(eml_file, 'rb') as f:
         msg = email.message_from_binary_file(f, policy=default)
+
+    html_content = msg.get_body(preferencelist=('html')).get_content()
+
+    # Extraire les images et les encoder en base64
+    for part in msg.iter_attachments():
+        if part.get_content_type() in ['image/jpeg', 'image/png', 'image/gif']:
+            image_data = part.get_payload(decode=True)
+            image_base64 = base64.b64encode(image_data).decode('utf-8')
+            image_name = part.get_filename()
+            html_content = html_content.replace(image_name, f'data:{part.get_content_type()};base64,{image_base64}')
+
     with open(html_file, 'w', encoding='utf-8') as f:
-        f.write(msg.get_body(preferencelist=('html')).get_content())
+        f.write(html_content)
 
 def main():
     # Demander à l'utilisateur de spécifier le chemin du fichier EML
